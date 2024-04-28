ad917050e72a9c808995f9b426a6744877539586
Ana Luiza <alclim02@gmail.com>
2024-04-28 16:42:50 -0400

    register changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,59 +1,53 @@
-
 import { loginRequest, getUserProfile } from "./requests.js";
 
-function handleLogin(){
-    const loginFields = document.querySelectorAll(".input-login");
-    const loginButton = document.querySelector("#button-login");
+function handleLogin() {
+  const loginFields = document.querySelectorAll(".input-login");
+  const loginButton = document.querySelector("#button-login");
 
-    const loginBody = {};
-    
-    
-    loginButton.addEventListener("click", async (buttonEvent) => {
-        let count = 0;
-        
-        buttonEvent.preventDefault();
-        
-        loginFields.forEach((input) => {
-            if(input.value == ""){
-                count++
-            }
-            
-            loginBody[input.name] = input.value
-            
-        })
-        
-        if(count !== 0){
-            return alert("por favor, preencha todos os campos");
-        } else {
-            const userToken = await loginRequest(loginBody);
+  const loginBody = {};
 
-            loginFields.forEach((input) => {
-                input.value = "";
-            });
+  loginButton.addEventListener("click", async (buttonEvent) => {
+    let count = 0;
 
-            if (userToken) {
-                localStorage.setItem("@petinfo:token", userToken);
-                const userProfile = await getUserProfile();
-                const userId = userProfile.id;
-                localStorage.setItem("@petinfo:userId", userId);
-                window.location = "/src/pages/dashboard.html";
-                return(userToken);
-            }
-        }
-    })
-    
+    buttonEvent.preventDefault();
+
+    loginFields.forEach((input) => {
+      if (input.value == "") {
+        count++;
+      }
+
+      loginBody[input.name] = input.value;
+    });
+
+    if (count !== 0) {
+      return alert("por favor, preencha todos os campos");
+    } else {
+      const userToken = await loginRequest(loginBody);
+
+      loginFields.forEach((input) => {
+        input.value = "";
+      });
+
+      if (userToken) {
+        localStorage.setItem("@petinfo:token", userToken);
+        const userProfile = await getUserProfile();
+        const userId = userProfile.id;
+        localStorage.setItem("@petinfo:userId", userId);
+        window.location = "/src/pages/dashboard.html";
+        return userToken;
+      }
+    }
+  });
 }
 
 handleLogin();
 
-
 function handleRedirectToSignUp() {
-    
-    const redirectButton = document.querySelector("#button-signup-redirect");
+  const redirectButton = document.querySelector("#button-signup-redirect");
 
-    redirectButton.addEventListener("click", () => {
-        window.location = "./src/pages/cadastro.html";
-    })
+  redirectButton.addEventListener("click", () => {
+    window.location = "./src/pages/register.html";
+  });
 }
 
 handleRedirectToSignUp();
